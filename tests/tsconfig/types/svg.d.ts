91e6393cb6136683d81612c2d7d3749c03f279ea
Alexey Plutalov <demiazz.py@gmail.com>
2023-07-03 23:08:18 +0300

    chore: add `eslint`, `prettier` and predefined TypeScript configs (#2)
    * build: add `prettier` and `@tabula/prettier-config` packages
    
    * chore: configure `prettier`
    
    * chore: add `prettier` to `lefthook` config
    
    * build: add `eslint` and `@tabula/eslint-config`
    
    * chore: configure `eslint`
    
    * build: add `lint` npm script
    
    * ci: add `lint` to the quality composite action
    
    * build: add `@tabula/typescript-config`
    
    * build: use predefined TypeScript configs
    
    * chore: add changesets
    
    * build: fix TypeScript configs
    
    * build: regenerate `pnpm-lock.yaml`
    
    * chore: update `.eslintignore`

--- tests/tsconfig/types/svg.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.svg' {
-  import type { FC, SVGProps } from 'react';
-
-  export const ReactComponent: FC<SVGProps<SVGSVGElement>>;
-
-  const url: string;
-
-  export default url;
-}
